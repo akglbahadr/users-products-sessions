4f948c6693027ccefe30ebf10958a2d310b24e8b
akglbahadr <akgl.bahadr@gmail.com>
2022-01-22 15:28:08 +0300

    Structure of validate is fixed

--- src/middleware/validateResource.ts
+++ src/middleware/validateResource.ts
@@ -1,23 +1,26 @@
+import { Request, Response, NextFunction } from "express"
+import {AnyZodObject} from 'zod';
+
 //middleware
 //when a req comes in, we're gonna provide a schema in the middleware, and then its going to validate the requests against that schema
 //for example email and password, we are going to make sure they are going to be present and they are both strings
 //and along with that email is actually email
 
+
 //technique is called currying 
-import { Request, Response, NextFunction } from "express"
-import {AnyZodObject} from 'zod';
-
-const validate = (schema) => (req:Request, res:Response, next:NextFunction) => {
-
-    try{
-        schema.parse({
-            body: req.body,
-            query: req.query,
-            params: req.params
-        })
-    }catch(e:any){
-        return res.status(400).send(e.errors);
+const validate =(schema: AnyZodObject) => (
+    req: Request, 
+    res: Response, next: NextFunction) => {
+    try {
+      schema.parse({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+      next();
+    } catch (e: any) {
+      return res.status(400).send(e.errors);
     }
-}
+  };
 
 export default validate;
